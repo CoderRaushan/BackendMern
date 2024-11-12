6955ba01a40fea342688f031283bce0e66cc5179
CoderRaushan <raushankumar23082004@gmail.com>
2024-11-12 09:31:33 +0530

    update

--- jwt/JwtToken.js
+++ jwt/JwtToken.js
@@ -7,7 +7,7 @@ const jwtTokenFunction=(userId,name,email,res)=>
       maxAge: 24 * 60 * 60 * 1000, 
       httpOnly: true,              
       secure: true,                
-      sameSite: 'strict', 
+      sameSite: 'None', 
       path: '/',             
     });
 };
